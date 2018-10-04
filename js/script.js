3061738c2a9e70d0903dc74c04e1c9c9b8fb3b27
rossifr <franco.f.rossi@gmail.com>
2018-10-04 04:54:15 +0200

    10.4

--- js/script.js
+++ js/script.js
@@ -150,11 +150,14 @@ function listChannels() {
     //$('#channels ul').append("<li>New Channel</li>")
 
     // #8 five new channels
-    $('#channels ul').append(createChannelElement(yummy));
+/* $('#channels ul').append(createChannelElement(yummy));
     $('#channels ul').append(createChannelElement(sevencontinents));
     $('#channels ul').append(createChannelElement(killerapp));
     $('#channels ul').append(createChannelElement(firstpersononmars));
-    $('#channels ul').append(createChannelElement(octoberfest));
+    $('#channels ul').append(createChannelElement(octoberfest));*/
+    for(var i=0; i < channels.length; i++){
+        $('#channels ul').append(createChannelElement(channels[i]));
+    };
 }
 
 /**
